da2188528ee85379cc8adb26a06539a47366e5a4
Maadhav-Aneja <maadhav.aneja@gmail.com>
2021-12-11 14:26:15 +0800

    add

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
 import HomeScreen from "./screens/Home";
-import IssLocationScreen from "./screens/Iss_Location";
+import IssLocationScreen from "./screens/IssLocation";
 import MeteorScreen from "./screens/Meteors";
 
 const Stack = createStackNavigator();
